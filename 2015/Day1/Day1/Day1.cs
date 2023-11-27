d18ea91960ec930e42f9ef5c7088918dbb78c338
WJConnors <billy.connors@yahoo.co.uk>
2023-11-27 18:17:01 +0000

    Initial commit Including 2015 Day 1

---
+++ 2015/Day1/Day1/Day1.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+String text = File.ReadAllText(rootDirectory + "/text.txt");
+int i = 0;
+int j = 0;
+foreach (char c in text)
+{
+    /*if (c.Equals('('))
+    {
+        i++;
+    }
+    else if (c.Equals(')'))
+    {
+        i--;
+    }*/
+    if (c.Equals('('))
+    {
+        i++;
+    }
+    else if (c.Equals(')'))
+    {
+        i--;
+    }
+    j++;
+    if (i == -1)
+    {
+        break;
+    }
+}
+Console.WriteLine(j);
