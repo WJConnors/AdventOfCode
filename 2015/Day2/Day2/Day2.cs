eb0445e6fc1545a317fc3d8f730de446e3559096
WJConnors <billy.connors@yahoo.co.uk>
2023-11-28 20:10:47 +0000

    Complete Day 2 C# Solution

---
+++ 2015/Day2/Day2/Day2.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+String[] text = File.ReadAllLines(rootDirectory + "/text.txt");
+
+int totalPaper = 0;
+int totalRibbon = 0;
+int[] dimensions = new int[3];
+
+foreach (string line in text)
+{
+    int cur = 0;
+    string curStr = "";
+    for (int i = 0; i < line.Length; i++)
+    {
+        char c = line[i];
+        if ( c != 'x')
+        {
+            curStr += c;
+        }
+        if (c == 'x' || i == line.Length - 1)
+        {
+            dimensions[cur] = int.Parse(curStr);
+            cur++;
+            curStr = "";
+        }
+    }
+
+    int[] areas = [dimensions[0] * dimensions[1], dimensions[1] * dimensions[2], dimensions[2] * dimensions[0]];
+    int smallest = areas.Min();
+    totalPaper += (2 * areas[0]) + (2 * areas[1]) + (2 * areas[2]) + smallest;
+
+    Array.Sort(dimensions);
+    int perimeter = (2 * dimensions[0]) + (2 * dimensions[1]);
+    int volume = dimensions[0] * dimensions[1] * dimensions[2];
+    totalRibbon += perimeter + volume;
+}
+
+Console.WriteLine(totalPaper);
+Console.WriteLine(totalRibbon);
