e6b077e2f7951c3927a9e2b28de47717a9004f52
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-11-30 12:35:25 +0000

    Day 5 Part 2 Mid Debugging

--- 2015/Day5/Day5/Program.cs
+++ 2015/Day5/Day5/Program.cs
@@ -3,11 +3,14 @@ using System.Reflection;
 
 string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 String[] text = File.ReadAllLines(rootDirectory + "/text.txt");
+text = ["qjhvhtzxzqqjkmpb", "xxyxx", "uurcxstgmygtbstg", "ieodomkazucvgmuy"];
+text = ["qjhvhtzxzqqjkmpb"];
 
 int nice = 0;
 
 foreach (string line in text)
 {
+    Console.WriteLine(line);
     Boolean twice = false;
     Boolean repeats = false;
     List<String> pairs = new List<String>();
@@ -15,24 +18,21 @@ foreach (string line in text)
 
     foreach (char c in line)
     { 
+        Console.WriteLine("next char: " + c);
         if ( c == previous[0]) { repeats = true; }
+        if (pairs.Contains(new string(([previous[1], c])))) { twice = true;}
+        Console.WriteLine(new string([previous[1], c]));
 
         pairs.Add(new string(previous));
 
         previous[0] = previous[1];
         previous[1] = c;
-
+        Console.WriteLine(new string(previous));
         pairs.Add(new string(previous));
     }
 
-    foreach (String pair in pairs)
-    {
-        if (line.Contains(pair)) {
-            twice = true;
-        }
-    }
-
     if (twice && repeats) { nice++; }
+    Console.WriteLine(nice);
 
 }
 
