ca16364edfa13bdcc13fe3c9acc3afddba938f2b
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-04 11:12:24 +0000

    2023 Day 4 Setup

---
+++ 2023/Day4/Day4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+String[] text = File.ReadAllLines(rootDirectory + "/text.txt");
+
+int total = 0;
+
+foreach (string line in text)
+{
+    string winning = line.Substring(line.IndexOf(':', line.IndexOf('|')));
+    string chosen = line.Substring(line.IndexOf('|') + 1);
+}
