a7a959579c8544692d19cbe5c75e847f118dc936
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-05 12:32:25 +0000

    2023 Day 5 Continuation

--- 2023/Day5/Day5/Program.cs
+++ 2023/Day5/Day5/Program.cs
@@ -12,28 +12,46 @@ foreach (string str in seedLine.Split(' '))
 }
 text.RemoveAt(0);
 
-List<List<(int, int)>> map = new List<List<(int, int)>>();
+List<Dictionary<int, int>> map = new List<Dictionary<int, int>>();
 int curMap = 0;
+AddNewMap();
+Boolean added = false;
 foreach (string line in text)
 {
-    if (line == "")
-    {
-        if (map[curMap].Any())
-        {
-            curMap++;
-        }
-        continue;
-    }
-    else if (!Char.IsNumber(line, 0))
-    {
-        map[curMap] = new List<(int, int)>();
-    } else
-    {
+    Console.WriteLine(line);
+    if (line == "") continue;
+    if (Char.IsNumber(line, 0)) {
         string[] strNums = line.Split(' ');
         int[] nums = Array.ConvertAll(strNums, int.Parse);
-        for (int i  = 0; i < nums[2]; i++)
+        for (int i = 0; i < nums[2]; i++)
         {
-            map[curMap].Add((nums[0] + i, nums[1]  + i));
+            if (map[curMap].ContainsKey(nums[1] + i))
+            {
+                map[curMap][nums[1] + i] = nums[0] + i;
+            }
         }
+        added = true;
+    } else if (added)
+    {
+        Console.WriteLine("Added");
+        curMap++;
+        added = false;
+    }
+}
+
+foreach (Dictionary<int, int> dict in map)
+{
+    for (int x = 0; x < dict.Count; x++)
+    {
+        Console.WriteLine("{0} and {1}", dict.Keys.ElementAt(x), dict[dict.Keys.ElementAt(x)]);
+    }
+}
+
+void AddNewMap()
+{
+    map.Add(new Dictionary<int, int>());
+    foreach (int seed in seeds)
+    {
+        map[curMap].Add(seed, seed);
     }
 }
