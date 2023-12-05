9ad6ab06cb1f3a8219c00886c56246a75978f867
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-05 15:28:31 +0000

    2023 Day 5 Beginning of Optimisation For Task 2

--- 2023/Day5/Day5/Program.cs
+++ 2023/Day5/Day5/Program.cs
@@ -1,11 +1,87 @@
 ﻿using System.Reflection;
 
 string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-List<string> text = new List<string>(File.ReadAllLines(rootDirectory + "/text.txt"));
+List<string> text = new List<string>(File.ReadAllLines(rootDirectory + "/test.txt"));
 
-string seedLine = text[0].Substring(text[0].IndexOf(':') + 2);
+string seedLineStr = text[0].Substring(text[0].IndexOf(':') + 2);
 
-List<double> seeds = new List<double>();
+
+List<double> seedLine = new List<double>();
+foreach (string str in seedLineStr.Split(' '))
+{
+    seedLine.Add(double.Parse(str));
+}
+text.RemoveAt(0);
+
+List<(double, double)> seedPairs = new List<(double, double)>();
+for (int i = 0; i < seedLine.Count - 1; i = i + 2)
+{
+    seedPairs.Add((seedLine[i], seedLine[i + 1]));
+}
+
+List < List < (double, double, double)>> maps = new List<List<(double, double, double)>>();
+int curMap = 0;
+maps.Add(new List<(double, double, double)>());
+Boolean added = false;
+foreach (string line in text)
+{
+    if (line == "") continue;
+    if (Char.IsNumber(line, 0))
+    {
+        string[] strNums = line.Split(' ');
+        double[] nums = Array.ConvertAll(strNums, double.Parse);
+        maps[curMap].Add((nums[0], nums[1], nums[2]));
+        added = true;
+    } else if (added)
+    {
+        curMap++;
+        maps.Add(new List<(double, double, double)>());
+    }
+}
+
+double lowest = 0;
+foreach ((double, double) pair in seedPairs)
+{
+    for (int i = 0; i < pair.Item2; i++)
+    {
+        double seed = pair.Item1 + i;
+        foreach (List<(double, double, double)> map in maps)
+        {
+            foreach ((double, double, double) values in map)
+            {
+                if (i >= values.Item2 && i <= values.Item2 + values.Item3)
+                {
+                    seed = values.Item1 + (i - values.Item1);
+                    break;
+                }
+            }
+        }
+        if (lowest == 0 || i < lowest)
+        {
+            lowest = seed;
+        }
+    }
+}
+
+Console.WriteLine(lowest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*List<double> seeds = new List<double>();
 foreach (string str in seedLine.Split(' '))
 {
     seeds.Add(double.Parse(str));
@@ -76,4 +152,4 @@ void AddNewMap()
             }
         }
     }
-}
+}*/
