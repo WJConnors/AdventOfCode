fb6d8ecbc366d938f88565b0d87a5a2f4baf8fc7
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-06 12:36:43 +0000

    2023 Day6 Initial Setup

---
+++ 2023/Day6/Day6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+String[] text = File.ReadAllLines(rootDirectory + "/text.txt");
+
+string time = text[0];
+string distance = text[1];
+List<int> times = new List<int>();
+List<int> distances = new List<int>();
+
+for (int i = 0; i < time.Length; i++)
+{
+    List<char> tempTime = new List<char>();
+    if (char.IsNumber(time[i]))
+    {
+        tempTime.Add(time[i]);
+        if (char.IsNumber(time[++i]))
+        {
+            tempTime.Add(time[i]);
+        }
+        times.Add(int.Parse(new string(tempTime.ToArray())));
+    }
+
+    List<char> tempDistance = new List<char>();
+    if (char.IsNumber(distance[i]))
+    {
+        tempDistance.Add(distance[i]);
+        if (char.IsNumber(distance[++i]))
+        {
+            tempTime.Add(distance[i]);
+        }
+        distances.Add(int.Parse(new string(tempDistance.ToArray())));
+    }
+}
