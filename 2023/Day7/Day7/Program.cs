9c4dd271aea4be8025eba10117cbc9dca0fd33dd
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-07 13:10:21 +0000

    2023 Day 7 Begun Strength Function

--- 2023/Day7/Day7/Program.cs
+++ 2023/Day7/Day7/Program.cs
@@ -1,7 +1,8 @@
-﻿using System.Reflection;
+﻿using System.ComponentModel;
+using System.Reflection;
 
 string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-String[] text = File.ReadAllLines(rootDirectory + "/test.txt");
+string[] text = File.ReadAllLines(rootDirectory + "/test.txt");
 
 List<(string, int)> input = new List<(string, int)>();
 
@@ -9,4 +10,27 @@ foreach (string line in text)
 {
     string[] split = line.Split(" ");
     input.Add((split[0], int.Parse(split[1])));
+}
+
+char Strength(string hand)
+{
+    List<(char, int)> card = new List<(char, int)>();
+    foreach (char c in hand)
+    {
+        Boolean found = false;
+        for (int i = 0; i < card.Count; i++)
+        {            
+            if (c == card[i].Item1)
+            {
+                card[i] = (card[i].Item1, card[i].Item2 + 1);
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            card.Add((c, 1));
+        }
+    }
+
+    return '0';
 }
