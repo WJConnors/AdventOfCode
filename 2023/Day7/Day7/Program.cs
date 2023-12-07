35a696954007e0bc1173decdde02ffbea78f2c7c
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-07 09:12:59 +0000

    2023 Day 7 Setup

---
+++ 2023/Day7/Day7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+String[] text = File.ReadAllLines(rootDirectory + "/test.txt");
+
+List<(string, int)> input = new List<(string, int)>();
+
+foreach (string line in text)
+{
+    string[] split = line.Split(" ");
+    input.Add((split[0], int.Parse(split[1])));
+}
