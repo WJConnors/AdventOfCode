accd424bbbbf3acd6b2e189302d92b7a097cfd98
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-08 10:09:44 +0000

    2023 Day 8 Part 1 and Part 2 Attempt

--- 2023/Day8/Day8/Program.cs
+++ 2023/Day8/Day8/Program.cs
@@ -13,15 +13,32 @@ foreach (string line in text)
     maps.Add((line.Substring(0, 3), line.Substring(7, 3), line.Substring(12, 3)));
 }
 
-string nextDirection = maps[0].Item1;
+List<string> nextDirections = new List<string>();
+foreach ((string, string, string) map in maps)
+{
+    if (map.Item1[2] == 'A')
+    {
+        nextDirections.Add(map.Item1);
+    }
+}
+
 int i = 0;
 int steps = 0;
+bool completed = false;
 
-while (nextDirection != "ZZZ")
+while (!completed)
 {
-    if (i == directions.Length)
+    for (int j = 0; j < nextDirections.Count; j++)
     {
-        i = 0;
+        int index = maps.FindIndex(x => x.Item1 == nextDirections[j]);
+        nextDirections[j] = directions[i] == 'L' ? maps[index].Item2 : maps[index].Item3;
     }
-    int index = maps.FindIndex(x => x.Item1 == nextDirection);
+    completed = true;
+    foreach (string direction in nextDirections)
+    {
+        if (direction[2] != 'Z') { completed = false; }
+    }
+    steps++;
+    i = i == directions.Length - 1 ? 0 : i + 1;
 }
+Console.WriteLine(steps);
