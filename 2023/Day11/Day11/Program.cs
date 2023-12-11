488a45dd6809c83a3489d8486230a528fc3575c3
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-11 09:12:11 +0000

    Day 11 Setup

---
+++ 2023/Day11/Day11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Reflection;
+using System.Xml.Linq;
+
+string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+List<string> text = new List<string>(File.ReadAllLines(rootDirectory + "/test.txt"));
+
+for (int i = 0; i < text.Count; i++)
+{
+    
+}
