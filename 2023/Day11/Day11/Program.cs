d1d6c3eff151c6061f513e4c9a018e05523b0944
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-11 13:57:05 +0000

    2023 Day 11 Completed Solution

--- 2023/Day11/Day11/Program.cs
+++ 2023/Day11/Day11/Program.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using System.Xml.Linq;
 
 string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-List<string> text = new List<string>(File.ReadAllLines(rootDirectory + "/test.txt"));
+List<string> text = new List<string>(File.ReadAllLines(rootDirectory + "/text.txt"));
 
 List<int> emptyRows = [];
 List<int> emptyCols = [];
@@ -18,27 +18,7 @@ for (int i = 0; i < text[0].Length; i++)
     foreach (string line in text) { if (line[i] == '#') { galFound = true; } }
     if (!galFound) { emptyCols.Add(i); }
 }
-int inserted = 0;
-foreach (int i in emptyRows)
-{
-    for (int j = 0; j < 10; j++)
-    {
-        text.Insert(i + inserted, text[i + inserted]);
-        inserted++;
-    }
-}
-inserted = 0;
-foreach (int i in emptyCols)
-{
-    for (int j = 0; j < text.Count; j++)
-    {
-        for (int k = 0; k < 10; k++)
-        {
-            text[j] = text[j].Insert(i + inserted + k, ".");
-        }
-    }
-    inserted++;
-}
+int mult = 999999;
 
 List<(int, int)> galaxies = [];
 for (int i = 0; i < text.Count; i++)
@@ -52,19 +32,56 @@ for (int i = 0; i < text.Count; i++)
     }
 }
 
-foreach (string line in text)
-{
-    Console.WriteLine(line);
-}
-
-int total = 0;
+double total = 0;
 foreach ((int, int) i in galaxies)
 {
     foreach ((int, int) j in galaxies)
     {
         if (galaxies.IndexOf(i) <= galaxies.IndexOf(j)) { continue; }
-        total += (Math.Abs(i.Item1 - j.Item1) + Math.Abs(i.Item2 - j.Item2));
+        int xDif = Math.Abs(i.Item1 - j.Item1);
+        int yDif = Math.Abs(i.Item2 - j.Item2);
+        int xNew = xDif;
+        int yNew = yDif;
+        foreach (int k in emptyRows)
+        {
+            if (Enumerable.Range(Math.Min(i.Item1, j.Item1), xDif).Contains(k))
+            {
+                xNew += mult;
+            }
+        }
+        foreach (int k in emptyCols)
+        {
+            if (Enumerable.Range(Math.Min(i.Item2, j.Item2), yDif).Contains(k))
+            {
+                yNew += mult;
+            }
+        }
+        total += xNew + yNew;
     }
 }
 
 Console.WriteLine(total);
+
+
+/*int inserted = 0;
+foreach (int i in emptyRows)
+{
+    for (int j = 0; j < mult; j++)
+    {
+        text.Insert(i + inserted, text[i + inserted]);
+        inserted++;
+    }
+}
+Console.WriteLine("ins rows");
+inserted = 0;
+foreach (int i in emptyCols)
+{
+    for (int j = 0; j < text.Count; j++)
+    {
+        for (int k = 0; k < mult; k++)
+        {
+            text[j] = text[j].Insert(i + inserted + k, ".");
+        }
+    }
+    inserted += mult;
+}*/
