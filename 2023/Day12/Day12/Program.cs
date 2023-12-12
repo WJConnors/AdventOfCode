4721004dd86b552b4d94e2dbd285dbc11827f851
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-12 09:14:47 +0000

    Day 12 Setup

---
+++ 2023/Day12/Day12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+List<string> text = new List<string>(File.ReadAllLines(rootDirectory + "/test.txt"));
+
+List<string> gears = [];
+List<string> strGroups = [];
+List<List<int>> groups = [];
+foreach (string line in text)
+{
+    string[] splitLine = line.Split(" ");
+    gears.Add(splitLine[0]);
+    strGroups.Add(splitLine[1]);
+}
+for (int i = 0; i < strGroups.Count; i++)
+{
+    string[] splitgroups = strGroups[i].Split(",");
+    groups.Add(new List<int>());
+    foreach (string group in splitgroups)
+    {
+        groups[i].Add(int.Parse(group));
+    }
+}
+
+for (int i = 0; i < gears.Count; i ++)
+{
+    Console.WriteLine(gears[i] + " " + strGroups[i]);
+}
