89368b19bcce63f84eeea8b9ceb564571ce96851
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-12 11:32:53 +0000

    2023 Day 12 Messing with IEnumerables

--- 2023/Day12/Day12/Program.cs
+++ 2023/Day12/Day12/Program.cs
@@ -24,5 +24,71 @@ for (int i = 0; i < strGroups.Count; i++)
 
 for (int i = 0; i < gears.Count; i ++)
 {
-    Console.WriteLine(gears[i] + " " + strGroups[i]);
+    Console.WriteLine(gears[i]);
+    List<int> arrangement = Arrangement(gears[i]);
+    foreach (int j in arrangement)
+    {
+        Console.WriteLine(j);
+    }
+    Console.WriteLine();
+}
+
+
+
+List<int> Arrangement(string gear)
+{
+    List<int> ints = [];
+
+    bool broken = false;
+    foreach( char c in gear)
+    {
+        if (c == '#')
+        {
+            if (!broken)
+            {
+                broken = true;
+                ints.Add(1);
+                continue;
+            }
+            ints[ints.Count - 1] = ints.Last() + 1;
+        } else
+        {
+            broken = false;
+        }
+    }
+
+    return ints;
+}
+
+List<string> Combinations(string gear, List<int> group)
+{
+    List<string> combinations = [];
+
+    int totalBroken = group.Sum();
+    int foundBroken = gear.Count(x => x == '#');
+    string questions = "";
+    for (int i = 0; i < gear.Length; i++)
+    {
+        if (gear[i] == '?')
+        {
+            questions += i.ToString();
+        }
+    }
+    int toAdd = totalBroken - foundBroken;
+    if (questions.Count() > 0)
+    {
+        combinations = intCombos.Combinations(questions, foundBroken);
+    }
+
+    return combinations;
+}
+
+static class intCombos
+{
+    public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> elements, int k)
+    {
+        return k == 0 ? new[] { new T[0] } :
+          elements.SelectMany((e, i) =>
+            elements.Skip(i + 1).Combinations(k - 1).Select(c => (new[] { e }).Concat(c)));
+    }
 }
