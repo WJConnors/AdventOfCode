95b60ecf42ea2ff065a4f9449a732b513775aa0a
WJConnors <billy.connors@yahoo.co.uk>
2023-12-13 20:01:17 +0000

    2023 Day 12 Completed Solutions

--- 2023/Day13/Day13/Program.cs
+++ 2023/Day13/Day13/Program.cs
@@ -3,7 +3,7 @@ using System.Reflection.PortableExecutable;
 using System.Text;
 
 string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-string[] text = File.ReadAllLines(rootDirectory + "/test.txt");
+string[] text = File.ReadAllLines(rootDirectory + "/text.txt");
 
 List<List<string>> input = [];
 input.Add([]);
@@ -18,68 +18,93 @@ foreach (string line in text)
     }
 }
 
-int total = 0;
+
+int total1 = 0;
+List<(char, int)> reflections = [];
+foreach (List<string> section in input)
+{
+    int horiVal = HoriReflection(section, -1);
+    if (horiVal != -1)
+    {
+        reflections.Add(('h', horiVal));
+        total1 += (horiVal + 1) * 100;
+    } else if (VertReflection(section, -1) != -1)
+    {
+        reflections.Add(('v', VertReflection(section, -1)));
+        total1 += VertReflection(section, -1) + 1;
+    }
+}
+
+Console.WriteLine(total1);
+
+int total2 = 0;
 for (int i = 0; i < input.Count; i++)
 {
     bool found = false;
     List<string> temp = new List<string>(input[i]);
     for (int j = 0; j < temp.Count; j++)
     {
-        for (int k = j + 1; k < temp[j].Length; k++)
+        for (int k = 0; k < temp[j].Length; k++)
         {
             char tempChar = temp[j][k];
             if (tempChar == '#')
             {
-                char[] tempArray = temp[j].ToCharArray();
-                tempArray[k] = '.';
-                temp[j] = new string(tempArray);
+                char[] tempArrayRocks = temp[j].ToCharArray();
+                tempArrayRocks[k] = '.';
+                temp[j] = new string(tempArrayRocks);
             } else
             {
-                char[] tempArray = temp[j].ToCharArray();
-                tempArray[k] = '#';
-                temp[j] = new string(tempArray);
+                char[] tempArrayAsh = temp[j].ToCharArray();
+                tempArrayAsh[k] = '#';
+                temp[j] = new string(tempArrayAsh);
             }
-            int hr = HoriReflection(temp);
-            int vr = VertReflection(temp);
+            int original = -1;
+            if (reflections[i].Item1 == 'h')
+            {
+                original = reflections[i].Item2;
+            } 
+            int hr = HoriReflection(temp, original);
+            if (reflections[i].Item1 == 'v')
+            {
+                original = reflections[i].Item2;
+            }
+            int vr = VertReflection(temp, original);
             if (hr != -1)
             {
-                total += (hr + 1) * 100;
-                found = true;
-                break;
+                if (!(reflections[i].Item1 == 'h' && reflections[i].Item2 == hr))
+                {
+                    total2 += (hr + 1) * 100;
+                    found = true;
+                    break;
+                }
             }
             if (vr != -1)
             {
-                total += vr + 1;
-                found = true;
-                break;
+                if (!(reflections[i].Item1 == 'v' && reflections[i].Item2 == vr))
+                {
+                    total2 += vr + 1;
+                    found = true;
+                    break;
+                }
             }
             char[] tempArray = temp[j].ToCharArray();
             tempArray[k] = tempChar;
             temp[j] = new string(tempArray);
         }
-        if (found) { break; }
+        if (found) { break; }        
+    }
+    if (!found)
+    {
+        Console.WriteLine(i);
     }
 }
+Console.WriteLine(total2);
 
-/*int total = 0;
-foreach (List<string> section in input)
-{
-    int horiVal = HoriReflection(section);
-    if (horiVal != -1)
-    {
-        total += (horiVal + 1) * 100;
-    } else if (VertReflection(section) != -1)
-    {
-        total += VertReflection(section) + 1;
-    } else
-    {
-        Console.WriteLine("Error");
-    }
-}*/
 
-Console.WriteLine(total);
 
-int HoriReflection (List<string> s)
+
+
+int HoriReflection (List<string> s, int original)
 {
     int found = -1;
 
@@ -100,14 +125,14 @@ int HoriReflection (List<string> s)
                 j -= 1;
                 k += 1;
             }
-            if (found != -1) { break; }
+            if (found != -1 && found != original) { break; }
         }
     }
 
     return found;
 }
 
-int VertReflection (List<string> s)
+int VertReflection (List<string> s, int original)
 {
     int found = -1;
     List<string> n = [];
@@ -122,7 +147,7 @@ int VertReflection (List<string> s)
         n.Add(sb.ToString());
     }
 
-    found = HoriReflection(n);
+    found = HoriReflection(n, original);
 
     return found;
 }
