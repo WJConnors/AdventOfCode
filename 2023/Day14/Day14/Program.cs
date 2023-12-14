67ac12afbcea325adad745db05303449c90c9463
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-14 16:36:25 +0000

    2023 Day 14 Task 1 Solution

--- 2023/Day14/Day14/Program.cs
+++ 2023/Day14/Day14/Program.cs
@@ -7,12 +7,32 @@ for  (int i = 0; i < text[0].Length; i++)
 {
     for (int j = 1; j < text.Length; j++)
     {
-        char temp1 = text[j][i];
-        char temp2 = text[i][j - 1];
-
-        if (temp1 == 'O' && temp2 == '.')
+        if (j < 1) { j = 1;}
+        if (text[j][i] == 'O' && text[j - 1][i] == '.')
         {
+            char[] arr1 = text[j].ToCharArray();
+            char[] arr2 = text[j - 1].ToCharArray();
 
+            arr1[i] = '.';
+            arr2[i] = 'O';
+
+            text[j] = new string(arr1);
+            text[j - 1] = new string(arr2);
+            j -= 2;
         }
     }
 }
+
+int total = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    foreach (char c in text[i])
+    {
+        if (c == 'O')
+        {
+            total += text.Length - i;
+        }
+    }
+}
+
+Console.WriteLine(total);
