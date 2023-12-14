ec7c72cf781464ed0f892187412b0c68e01fbee4
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-14 15:08:06 +0000

    2023 Day 14 Setup

---
+++ 2023/Day14/Day14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+String[] text = File.ReadAllLines(rootDirectory + "/text.txt");
+
+for  (int i = 0; i < text[0].Length; i++)
+{
+    for (int j = 1; j < text.Length; j++)
+    {
+        char temp1 = text[j][i];
+        char temp2 = text[i][j - 1];
+
+        if (temp1 == 'O' && temp2 == '.')
+        {
+
+        }
+    }
+}
