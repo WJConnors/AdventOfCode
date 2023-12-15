d2d7e8c81fcf2bb4799bbc13d453ce1ab7a4d8c3
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-15 10:15:15 +0000

    2023 Day 15 Task 2 Setup

--- 2023/Day15/Day15/Program.cs
+++ 2023/Day15/Day15/Program.cs
@@ -1,6 +1,44 @@
 ﻿using System.Reflection;
+using System.Text;
 
-string? rootDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-String text = File.ReadAllText("test.txt");
+String text = File.ReadAllText("text.txt");
 
 string[] strings = text.Split(",");
+List<List<(string, int)>> boxes = [];
+List<(string,string)> input = [];
+for (int i = 0; i < 256; i++)
+{
+    boxes.Add([]);
+}
+foreach (string s in strings)
+{
+    List<char> chars1 = [];
+    List<char> chars2 = [];
+    bool found = false;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] == '=' || s[i] == '-') {found = true; }
+        if (!found)
+        {
+            chars1.Add(s[i]);
+        } else
+        {
+            chars2.Add(s[i]);
+        }
+    }
+    input.Add((new string(chars1.ToArray()), new string(chars2.ToArray())));
+}
+
+
+int Hash (string s)
+{
+    int curVal = 0;
+    foreach (char c in s)
+    {
+        int ascii = (int)c;
+        curVal += ascii;
+        curVal *= 17;
+        curVal %= 256;
+    }
+    return curVal;
+}
