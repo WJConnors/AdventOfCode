fba7d065a205750158271272b12918e56cfa9cd4
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-15 15:16:09 +0000

    2023 Day 15 Part 2 Incomplete

--- 2023/Day15/Day15/Program.cs
+++ 2023/Day15/Day15/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Text;
 
-String text = File.ReadAllText("text.txt");
+String text = File.ReadAllText("test.txt");
 
 string[] strings = text.Split(",");
 List<List<(string, int)>> boxes = [];
@@ -29,6 +29,50 @@ foreach (string s in strings)
     input.Add((new string(chars1.ToArray()), new string(chars2.ToArray())));
 }
 
+foreach ((string, string) i in input)
+{
+    string label = i.Item1;
+    int box = Hash(label);
+    char operation = i.Item2[0];
+    Console.WriteLine(i);
+    if (operation == '=')
+    {
+        int focal = int.Parse(i.Item2[1..]);
+        if (boxes[box].Count == 0) { boxes[box].Add((label, focal)); Console.WriteLine("added"); continue; }
+        for (int j = 0; j < boxes[box].Count; j++)
+        {
+            if (boxes[box][j].Item1 == label)
+            {
+                Console.WriteLine("found");
+                boxes[box][j] = (label, focal);
+                break;
+            }
+            boxes[box].Add((label, focal));
+            Console.WriteLine("added");
+        }
+    } else
+    {
+        for (int j = 0; j < boxes[box].Count; j++)
+        {
+            if (boxes[box][j].Item1 == label)
+            {
+                boxes[box].RemoveAt(j);
+                break;
+            }
+        }
+    }
+}
+int total = 0;
+for (int i = 0; i < boxes.Count; i++)
+{
+    Console.WriteLine(boxes[i].Count);
+    for (int j = 0;j < boxes[i].Count; j++)
+    {
+        total += (i + 1) * (j + 1) * boxes[i][j].Item2;
+    }
+}
+
+Console.WriteLine(total);
 
 int Hash (string s)
 {
