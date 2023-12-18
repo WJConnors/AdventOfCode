2cebd3d56415aeef5276dde7078a18025524af28
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-18 10:12:04 +0000

    2023 Day 18 Slightly More Setup

--- 2023/Day18/Day18/Program.cs
+++ 2023/Day18/Day18/Program.cs
@@ -4,5 +4,10 @@
 
 foreach (string line in input)
 {
-    
+    char dir = line[0];
+    string dist = line.Substring(2, 1);
+    if (char.IsDigit(line[3]))
+    {
+        dist += line[3];
+    }
 }
