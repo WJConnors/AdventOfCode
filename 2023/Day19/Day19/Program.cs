d12052a7c12e5c964db25163bcff573a1e1a0668
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-19 13:34:55 +0000

    2023 Day 19 Update

--- 2023/Day19/Day19/Program.cs
+++ 2023/Day19/Day19/Program.cs
@@ -25,14 +25,89 @@ foreach (string line in workflows)
     {
         workflowRules.Last().Add(str);
     }
-    workflowRules.Last()[workflowRules.Last().Count - 1] = workflowRules.Last().Last().Remove(workflowRules.Last().Last().Length - 1, 1);
+    workflowRules.Last()[^1] = workflowRules.Last().Last().Remove(workflowRules.Last().Last().Length - 1, 1);
+}
+List<List<int>> rateVals = [];
+foreach (string line in ratings)
+{
+    List<string> strList = [.. line.Split(",")];
+    List<int> intList = [];
+    foreach (string str in strList)
+    {
+        string curVal = "";
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (char.IsDigit(str[i]))
+            {
+                curVal += str[i];
+            }
+        }
+        intList.Add(int.Parse(curVal));
+    }
+    rateVals.Add(intList);
 }
 
-foreach (List<string> str in workflowRules)
+int index = workflowRules.FindIndex(x => x[0] == "in");
+Console.WriteLine(index);
+List<string> curRules = workflowRules[index];
+string newFlow = "in";
+double total = 0;
+foreach (List<int> part in rateVals)
 {
-    foreach (string s in str)
+    for (int i = 0; i < curRules.Count; i++)
     {
-        Console.Write(s + " ");
+        bool found = false;
+        if (i == curRules.Count - 1)
+        {
+            found = true;
+            newFlow = curRules[i];
+            break;
+        }
+
+        int cat = CharToCat(curRules[i][0]);
+        char op = curRules[i][1];
+        string strVal = "";
+        for (int j = 2; j  < curRules.Count; j++)
+        {
+            if (char.IsDigit(curRules[i][j]))
+            {
+                strVal += curRules[i][j];
+            }
+        }
+        int val = int.Parse(strVal);
+        found = op switch
+        {
+            '<' => part[cat] < val,
+            '>' => part[cat] < val,
+            _ => throw new Exception("incorrect op: " + op),
+        };
+        if (found)
+        {
+            newFlow = curRules[i].Split(':')[1];
+            break;
+        }
     }
-    Console.WriteLine();
+    if (newFlow == "A")
+    {
+        total += part.Sum();
+        continue;
+    } else if ( newFlow == "R")
+    {
+        continue;
+    } else
+    {
+    }
+}
+
+static int CharToCat(char c)
+{
+    var cat = c switch
+    {
+        'x' => 0,
+        'm' => 1,
+        'a' => 2,
+        's' => 3,
+        _ => throw new Exception("incorrect chartocat: " + c),
+    };
+    return cat;
 }
