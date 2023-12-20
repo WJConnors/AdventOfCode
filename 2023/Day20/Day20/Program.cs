a599485a58314715929898e896e6ff2344c39e2e
WJConnors <37446973+WJConnors@users.noreply.github.com>
2023-12-20 12:33:35 +0000

    2023 Day 20 Setup

---
+++ 2023/Day20/Day20/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] input = [.. File.ReadAllLines("text.txt")];
+List<List<string>> inputs = [];
+foreach (string line in input)
+{
+    inputs.Add([.. line.Split(" ")]);
+}
+int bIndex = inputs.FindIndex(x => x[0] == "broadcaster");
+List<string> bOut = inputs[bIndex].GetRange(2, inputs[bIndex].Count - 2);
+inputs.RemoveAt(bIndex);
+for (int i = 0; i < bOut.Count - 1; i++)
+{
+    bOut[i] = bOut[i].Remove(bOut[i].Length - 1);
+}
+
+List<List<(string, bool, List<string>)>> flipflops = [];
+List<List<(string, List<(string, bool)>, List<string>)>>  conjunctions = [];
+
+foreach (List<string> lst in inputs)
+{
+    if (lst[0][0] == '%')
+    {
+
+    } else if (lst[0][0] == '&')
+    {
+
+    } else
+    {
+        throw new Exception("bad line start: " + lst[0][0]);
+    }
+}
